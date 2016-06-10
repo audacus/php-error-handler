1bf26eac0e819dee99a612b3fc645cdfe8f20efa
audacus <david.burkhart@hotmail.com>
2016-06-10 16:26:55 +0200

    ~updated from constructive-tube

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -3,9 +3,8 @@
 abstract class ErrorHandler {
 
 	public static function error($code, $message, $file, $line, array $context = null) {
-		// echo '<pre>'.print_r(func_get_args(),1).'</pre>';die();
-		static::exception(new ErrorException($message, $code, 0, $file, $line));
+		static::exception(new Exception($message, $code, new ErrorException($message, $code, 0, $file, $line)));
 	}
 
-	abstract public static function exception(Exception $e);
+	abstract public static function exception($e);
 }
